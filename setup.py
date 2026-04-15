50bee8385c436d2450e9ca1613f8ba8b9bf8029c
Manuel Tran <95863934+manuel-tran@users.noreply.github.com>
2026-04-16 00:13:23 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="phoenix",
+    version="1.0.0",
+    description="Phoenix - PyTorch",
+    packages=find_packages(exclude=[]),
+    install_requires=[
+        "timm==1.0.19",
+        "torch==2.6.0",
+        "torchvision==0.21.0",
+        "transformers==4.55.4",
+        "zuko==1.3.0",
+    ],
+)
