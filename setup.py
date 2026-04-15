177734d20cd9597f3e1ce1f393cd988135d499ad
Manuel Tran <95863934+manuel-tran@users.noreply.github.com>
2026-04-16 01:40:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     description="Phoenix - PyTorch",
     packages=find_packages(exclude=[]),
     install_requires=[
-        "timm==1.0.19",
-        "torch==2.6.0",
-        "torchvision==0.21.0",
-        "transformers==4.55.4",
-        "zuko==1.3.0",
+        #"timm==1.0.19",
+        #"torch==2.6.0",
+        #"torchvision==0.21.0",
+        #"transformers==4.55.4",
+        #"zuko==1.3.0",
     ],
 )
